2332038aa920f901210d0bc04c2ea1baf891dd16
qqaazz2 <61405036+qqaazz2@users.noreply.github.com>
2026-05-24 20:26:18 +0800

    feat:修改包名

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.dreader"
+    namespace = "io.github.qqaazz2.dreader"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.dreader"
+        applicationId = "io.github.qqaazz2.dreader"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/dreader/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dreader
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
